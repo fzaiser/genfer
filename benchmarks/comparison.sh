4623e7ca697028af139ad3fd557eaa9c752b0f51
Fabian Zaiser <fabian.zaiser@cs.ox.ac.uk>
2024-11-06 10:53:50 +0100

    Update internal comparison results

--- benchmarks/comparison.sh
+++ benchmarks/comparison.sh
@@ -4,17 +4,6 @@ cd ..
 cargo build --release --bins
 cd benchmarks/
 
-echo "Computing bounds for herman.sgcl ..."
-../target/release/residual herman.sgcl --limit 200 -u 30 > outputs/herman_residual.txt
-../target/release/geobound herman.sgcl --limit 200 -u 1 --objective tail > outputs/herman_bound_tail.txt
-../target/release/geobound herman.sgcl --limit 200 -u 30 --optimizer ipopt --optimizer linear --objective ev > outputs/herman_bound_moments.txt
-../target/release/geobound herman.sgcl --limit 200 -u 30 --optimizer ipopt --optimizer linear --objective total > outputs/herman_bound_probs.txt
-
-echo "Computing bounds for coupon-collector.sgcl ..."
-../target/release/residual coupon-collector.sgcl --limit 300 -u 80 > outputs/coupon_collector_residual.txt
-../target/release/geobound coupon-collector.sgcl --limit 300 -u 1 --objective tail > outputs/coupon_collector_bound_tail.txt
-../target/release/geobound coupon-collector.sgcl --limit 300 -u 80 --objective ev --optimizer ipopt --optimizer linear > outputs/coupon_collector_bound_moments.txt
-../target/release/geobound coupon-collector.sgcl --limit 300 -u 80 --objective total --optimizer ipopt --optimizer linear > outputs/coupon_collector_bound_probs.txt
 
 echo "Computing bounds for geo.sgcl ..."
 ../target/release/residual geo.sgcl --limit 200 -u 50 > outputs/geo_residual.txt
@@ -33,3 +22,15 @@ echo "Computing bounds for die_paradox.sgcl ..."
 ../target/release/geobound die_paradox.sgcl --limit 200 -u 1 -d 2 --objective tail > outputs/die_paradox_bound_tail.txt
 ../target/release/geobound die_paradox.sgcl --limit 200 -u 40 --objective ev > outputs/die_paradox_bound_moments.txt
 ../target/release/geobound die_paradox.sgcl --limit 200 -u 40 --keep-while --objective total > outputs/die_paradox_bound_probs.txt
+
+echo "Computing bounds for coupon-collector.sgcl ..."
+../target/release/residual coupon-collector.sgcl --limit 300 -u 80 > outputs/coupon_collector_residual.txt
+../target/release/geobound coupon-collector.sgcl --limit 300 -u 1 --objective tail > outputs/coupon_collector_bound_tail.txt
+../target/release/geobound coupon-collector.sgcl --limit 300 -u 80 --objective ev --optimizer ipopt --optimizer linear > outputs/coupon_collector_bound_moments.txt
+../target/release/geobound coupon-collector.sgcl --limit 300 -u 80 --objective total --optimizer ipopt --optimizer linear > outputs/coupon_collector_bound_probs.txt
+
+echo "Computing bounds for herman.sgcl ..."
+../target/release/residual herman.sgcl --limit 200 -u 30 > outputs/herman_residual.txt
+../target/release/geobound herman.sgcl --limit 200 -u 1 --objective tail > outputs/herman_bound_tail.txt
+../target/release/geobound herman.sgcl --limit 200 -u 30 --optimizer ipopt --optimizer linear --objective ev > outputs/herman_bound_moments.txt
+../target/release/geobound herman.sgcl --limit 200 -u 30 --optimizer ipopt --optimizer linear --objective total > outputs/herman_bound_probs.txt
